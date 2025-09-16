f1c5b01a008ab43d94033dec58cd3e5b78bd4254
Ncreate101 <87913557+Ncreate101@users.noreply.github.com>
2025-09-16 17:29:32 -0400

    Initial Commit

---
+++ src/main/java/us/xlumen/mapplane/Mapplane.java
@@ -0,0 +1,10 @@
+package us.xlumen.mapplane;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mapplane implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/us/xlumen/mapplane/client/MapplaneClient.java
@@ -0,0 +1,10 @@
+package us.xlumen.mapplane.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MapplaneClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
